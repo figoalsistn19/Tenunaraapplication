8de0fb416a3908e1ac156f9efb06ebeaebc8f609
DelianaRahmaAzahra <adeliarahma781@gmail.com>
2022-06-07 16:34:20 +0700

    Update project

--- app/src/main/java/com/example/tenunaraapplication/main/ui/ui/history/HistoryScanActivity.kt
+++ app/src/main/java/com/example/tenunaraapplication/main/ui/ui/history/HistoryScanActivity.kt
@@ -1,12 +1,15 @@
 package com.example.tenunaraapplication.main.ui.ui.history
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tenunaraapplication.R
+import com.example.tenunaraapplication.databinding.ActivityHistoryScanBinding
+import com.example.tenunaraapplication.main.ui.ui.home.HomeFragment
 import com.example.tenunaraapplication.remote.ScanHistory
 import com.google.firebase.firestore.*
 
@@ -16,6 +19,7 @@ class HistoryScanActivity : AppCompatActivity() {
     private lateinit var scanHistoryArrayList: ArrayList<ScanHistory>
     private lateinit var historyScanAdapter: HistoryScanAdapter
     private lateinit var db: FirebaseFirestore
+    private lateinit var binding: ActivityHistoryScanBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +36,12 @@ class HistoryScanActivity : AppCompatActivity() {
         recyclerView.adapter = historyScanAdapter
 
         eventChangeListener()
+
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this@HistoryScanActivity,HomeFragment::class.java)
+            startActivity(intent)
+        }
+
     }
 
     private fun eventChangeListener() {
